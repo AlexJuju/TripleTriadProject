7d7145157995053cc840fd2b3fcf4ffa44348404
AlexJuju <account4s@live.it>
2021-02-07 22:33:49 +0100

    Edit Glossary Screen/Controller

---
+++ src/controller/GlossaryController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.FlowPane;
+
+public class GlossaryController implements Controller {
+
+    @FXML
+    private FlowPane card_list;
+
+    @FXML
+    void previousScreen(ActionEvent event) {
+
+    }
+
+}
