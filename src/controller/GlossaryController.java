bd47109873cf9b4d638a4db9bb8381f20399b048
AlexJuju <account4s@live.it>
2021-02-09 18:52:45 +0100

    Editing Resources

--- src/controller/GlossaryController.java
+++ src/controller/GlossaryController.java
@@ -2,10 +2,14 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import logic.Game;
 import model.Card;
+
+import java.io.File;
 
 public class GlossaryController implements Controller {
 
@@ -13,12 +17,18 @@ public class GlossaryController implements Controller {
     private FlowPane card_list;
 
     public void initialize() {
-        Card cartaprova = new Card( 3, 4,2, 6, null);
+
+        File file = new File("resources/seed.jpg");
+        Image image = new Image(file.toURI().toString());
+        System.out.println( image );
+
+        Card cartaprova = new Card( 3, 4,2, 6, image);
         card_list.getChildren().add( cartaprova.getCardModel() );
         Card cartaprova1 = new Card( 3, 4,2, 6, null);
         card_list.getChildren().add( cartaprova1.getCardModel() );
         Card cartaprova2 = new Card( 3, 4,2, 6, null);
         card_list.getChildren().add( cartaprova2.getCardModel() );
+        card_list.getChildren().add( new ImageView( image ));
     }
 
     @FXML
